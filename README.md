30c87676efaad8d084259e31bd1385a381cfce72
Guill Lo <guill.bout@gmail.com>
2018-08-13 15:11:19 +0700

    Add ability to view words equivalence

---
+++ README.md
@@ -0,0 +1,4 @@
+# Features
+
+* Type English, Cantonese, uppercase, lowercase, accent, non-accent, intonations, whatever, your shit shows up in big letters.
+* So basically it shows you your English <-> Cantonese equivalent that you might have forgotten because Cantonese is hard.
---
+++ src/commands/list.php
@@ -0,0 +1,49 @@
+<?php
+
+use Alfred\Workflows\Workflow;
+
+require 'vendor/autoload.php';
+
+$workflow = new Workflow;
+
+$file = getenv('file');
+
+foreach (getLines($file) as $key => $line) {
+    if ($key === 0 || $key === 1 || empty($line)) {
+        continue;
+    }
+
+    $workflow->result()
+        ->uid(preg_replace("#[^a-zA-z ]#", "", (iconv('UTF-8', 'ASCII//IGNORE//TRANSLIT', "$line[1]-$line[2]"))))
+        ->title('🇺🇸️' .$line[2])
+        ->subtitle('🇲🇴️' .$line[1])
+        ->arg($line[1])
+        ->cmd("Show the shit above in big letters if you can't see clearly 🖕🏽️", $line[2])
+        ->largetype($line[2])
+        ->valid(true);
+}
+
+$term = trim($argv[1]);
+
+$workflow->filterResults(preg_replace("#[^a-zA-z ]#", "", (iconv('UTF-8', 'ASCII//IGNORE//TRANSLIT', $term))), 'uid');;
+
+echo $workflow->output();
+
+/**
+ * Functions
+ */
+
+function getLines($file)
+{
+    $handle = fopen($file, 'rb');
+
+    if (!handle) {
+        throw new Exception();
+    }
+
+    while (!feof($handle)) {
+        yield fgetcsv($handle, 0, '|');
+    }
+
+    fclose($handle);
+}
